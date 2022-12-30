91ef4eba20d260e90ec5cdfce2327d62e686ec69
Ismoilov Asasdbek <94944448+ismoilov299@users.noreply.github.com>
2022-12-30 18:39:00 +0500

    checkbox is clicked

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from turtle import delay
+
+from selenium.common import TimeoutException
+
 from keys.xpath_keys import *
 from time import sleep
 from selenium.webdriver import ActionChains
@@ -8,7 +12,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
-from fake_useragent import UserAgent
+
 
 
 def get_chromedriver(use_proxy=False, user_agent=None):
@@ -26,8 +30,8 @@ def get_chromedriver(use_proxy=False, user_agent=None):
 
 def login():
     try:
-        user_agent = UserAgent()
-        user_agent.update()
+        # user_agent = UserAgent()
+        # user_agent.update()
         driver = get_chromedriver()
         driver.maximize_window()
         driver.get("https://visa.vfsglobal.com/tur/en/pol/login")
@@ -37,10 +41,74 @@ def login():
         WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, x))).click()
         WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.ID, gmail))).send_keys("zafardavlatov2003@gmail.com")
         WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.ID, password))).send_keys('Davlatov3107!')
-        WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, sign_in))).click()
-        WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, important))).click()
-        element = driver.find_element(By.XPATH, important)
-        driver.execute_script("arguments[0].click();", element)
+        # WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, sign_in))).click()
+        # WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, important))).click()
+        # element = driver.find_element(By.XPATH, important)
+        # driver.execute_script("arguments[0].click();", element)
+
+
+
+        sleep(20)
+
+        ##########################################
+        # Click to the Recaptcha button and solve
+        ##########################################
+
+        WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,
+                                                                                   "iframe[title='reCAPTCHA']")))  # Write the title of the iframe that it is in. Because ReCaptcha checkbox is within an <iframe>
+        WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "div.recaptcha-checkbox-border"))).click()
+        print('''
+        ##########################################
+        # Click to the Recaptcha button and solve
+        ##########################################''')
+
+        # The user will manually solve Recaptcha here. Then, the code below will click on Sign In
+        sleep(12)  # Recaptcha can be easily solved within 12 seconds
+
+        driver.switch_to.default_content()  # To be able to switch between different Frames
+        WebDriverWait(driver, 40).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                                                    "button.mat-focus-indicator.btn.mat-btn-lg.btn-block.btn-brand-orange.mat-stroked-button.mat-button-base"))).click()
+
+        ##########################
+        # Schedule an appointment
+        ##########################
+
+        # If there is no slot for Istanbul region, return back to Dashboard; then try again (to be able to stay in the system)
+        for i in range(10000000000):  # We will check for 10000000000 times. This number is enough for 1 day
+            try:
+                #####################
+                # Confirmations page
+                #####################
+                sleep(12)  # Based on my experiments, 12 seconds should be enough
+
+                WebDriverWait(driver, 20).until(
+                    EC.element_to_be_clickable((By.XPATH, "//div[contains(.//text(),'before scheduling')]"))).click()
+                WebDriverWait(driver, 20).until(
+                    EC.element_to_be_clickable((By.XPATH, "//div[contains(.//text(), 'choose the correct' )]"))).click()
+
+                driver.set_window_size(100,
+                                       700)  # When the page is narrowed by the sides, the location of the button is changing and finally the click operation works
+                WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                                                            "button.mat-focus-indicator.btn.mat-btn-lg.btn-block.btn-brand-orange.mat-raised-button.mat-button-base"))).click()  # Button class içindeki yazıyı, boşlukları noktalarla değiştirerek yaz. Bekleme yapmadan da çalışmadı bu nedense, stackoverflow'da öyle çözmüş biri
+
+                ############################
+                # Terms and Conditions page
+                ############################
+                WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,
+                                                                            "//div[contains(.//text(), 'Continue Terms and Conditions' )]"))).click()  # Tıkladığında, inspect tarafında renklenen kısmın içindeki "id" değerini gir
+
+                WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                                                            "button.mat-focus-indicator.btn.mat-btn-lg.btn-block.btn-brand-orange.mat-stroked-button.mat-button-base"))).click()  # Button class içindeki yazıyı, boşlukları noktalarla değiştirerek yaz. Bekleme yapmadan da çalışmadı bu nedense, stackoverflow'da öyle çözmüş biri
+
+                driver.maximize_window()  # To easily observe the future operations on the page, turn the window into the maximized version
+
+            except TimeoutException:
+                WebDriverWait(driver, 20).until(
+                    EC.element_to_be_clickable((By.ID, "navbarDropdown"))).click()  # Click on "My Account"
+                WebDriverWait(driver, 20).until(
+                    EC.element_to_be_clickable((By.CLASS_NAME, "dropdown-item"))).click()  # Click on "Dashboard"
+
         # action = ActionChains(driver)
         # action.click(on_element=el)
         # action.perform()
@@ -50,4 +118,3 @@ def login():
 
 if __name__ == '__main__':
     login()
-
